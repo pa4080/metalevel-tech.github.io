1141b73f4c3893ef98b4badb2502a439f6ef3637
pa4080 <spas.z.spasov@gmail.com>
2022-03-08 19:21:20 +0200

    JS Assets

--- assets/js/change-header-color.js
+++ assets/js/change-header-color.js
@@ -4,23 +4,26 @@
     const rgb = [255, 0, 0];
 
     const node = document.querySelector('header.page-header');
+    let i = 0;
 
     // Randomly change to showcase updates
     setInterval(setContrast, 1000);
-    
+
     function setContrast() {
-      // Randomly update colours
-      rgb[0] = Math.round(Math.random() * 255);
-      rgb[1] = Math.round(Math.random() * 255);
-      rgb[2] = Math.round(Math.random() * 255);
-    
-      // http://www.w3.org/TR/AERT#color-contrast
-      const brightness = Math.round(((parseInt(rgb[0]) * 299) +
-                          (parseInt(rgb[1]) * 587) +
-                          (parseInt(rgb[2]) * 114)) / 1000);
-      const textColour = (brightness > 125) ? 'black' : 'white';
-      const backgroundColour = 'rgb(' + rgb[0] + ',' + rgb[1] + ',' + rgb[2] + ')';
-      $(node).css('color', textColour); 
-      $(node).css('background-color', backgroundColour);
+        // Randomly update colours
+        rgb[0] = Math.round(Math.random() * 255);
+        rgb[1] = Math.round(Math.random() * 255);
+        rgb[2] = Math.round(Math.random() * 255);
+
+        // http://www.w3.org/TR/AERT#color-contrast
+        const brightness = Math.round(((parseInt(rgb[0]) * 299) +
+                            (parseInt(rgb[1]) * 587) +
+                            (parseInt(rgb[2]) * 114)) / 1000);
+        const textColour = (brightness > 125) ? 'black' : 'white';
+        const backgroundColour = 'rgb(' + rgb[0] + ',' + rgb[1] + ',' + rgb[2] + ')';
+        $(node).css('color', textColour); 
+        $(node).css('background-color', backgroundColour);
+
+        console.log(i++);
     }
 })();
