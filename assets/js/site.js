a9e32110b2d7ee56604892cd93406cb41f35e8d3
pa4080 <spas.z.spasov@gmail.com>
2022-03-08 20:24:52 +0200

    Header

---
+++ assets/js/site.js
@@ -0,0 +1,29 @@
+// https://stackoverflow.com/questions/11867545/change-text-color-based-on-brightness-of-the-covered-background-area
+// https://css-tricks.com/using-javascript-to-adjust-saturation-and-brightness-of-rgb-colors/
+
+(function() {
+
+    const rgb = [255, 0, 0];
+
+    const node = document.querySelector('header.page-header');
+
+    // Randomly change to showcase updates
+    setInterval(setContrast, 2000);
+
+    function setContrast() {
+        // Randomly update colours
+        rgb[0] = Math.round(Math.random() * 255);
+        rgb[1] = Math.round(Math.random() * 255);
+        rgb[2] = Math.round(Math.random() * 255);
+
+        // http://www.w3.org/TR/AERT#color-contrast
+        const brightness = Math.round(((parseInt(rgb[0]) * 299) +
+                            (parseInt(rgb[1]) * 587) +
+                            (parseInt(rgb[2]) * 114)) / 1000);
+        const textColour = (brightness > 125) ? 'black' : 'white';
+        // const backgroundColour = 'rgb(' + rgb[0] + ',' + rgb[1] + ',' + rgb[2] + ')';
+        
+        $(node).css('color', textColour); 
+        // $(node).css('background-color', backgroundColour);
+    }
+})();
