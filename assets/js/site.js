4cdd519a7da90240b7bfd61db75929c683576c4a
pa4080 <spas.z.spasov@gmail.com>
2022-03-09 14:29:18 +0200

    markdown check-list statistics

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,5 +1,31 @@
 /**
+ * Create a statistic for pages which have GitHub MarkDown
+ * 'contains-task-list' | 'task-list' in Jekill,
+ * currently only the first task-list is handled.
  */
 (function() {
+   
+    const list = document.body.querySelectorAll('.task-list li input').length;
+    if (list) {
+        const passed = document.body.querySelectorAll('.task-list li input[checked]').length;
+        const output = `&nbsp;Tacks ${list}, Completed ${passed}, ${((passed / list) * 100).toFixed(0)}%&nbsp;`;
+    
+        const element = document.createElement('code');
+        element.style.position = 'absolute';
+        element.style.top = '-3rem';
+        element.style.right = '0';
+        element.style.border = '1px solid #ccc';
+        element.style.display = 'none';
+        element.style.transition = '1s';
+        element.innerHTML = output;
+        
+        const wrapper = document.createElement('div');
+        wrapper.style.position = 'relative';
+        wrapper.appendChild(element);
+        
+        const firstH1 = document.body.querySelector('main#content > h1:first-child');
+        firstH1.append(wrapper);
 
+        element.style.display = 'block';
+    }
 })();
