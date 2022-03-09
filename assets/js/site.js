69a531bb3330e7b3d3dd385a6e538830768bd217
pa4080 <spas.z.spasov@gmail.com>
2022-03-09 18:06:29 +0200

    .md task-list Statistics top position

--- assets/js/site.js
+++ assets/js/site.js
@@ -12,7 +12,7 @@
     
         const element = document.createElement('code');
         element.style.position = 'absolute';
-        element.style.top = '-3rem';
+        element.style.top = '-4rem';
         element.style.right = '0';
         element.style.border = '1px solid #ccc';
         element.innerHTML = output;
