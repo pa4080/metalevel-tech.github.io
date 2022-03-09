236224fd4d2b84d9a85857ed693440f616bb9b23
pa4080 <spas.z.spasov@gmail.com>
2022-03-09 10:59:45 +0200

    nav menu JS

--- assets/js/nav-sticky-position.js
+++ assets/js/nav-sticky-position.js
@@ -12,7 +12,7 @@
 
     const node = document.getElementById('nav');
     const stickyClass = 'sticky';
-    let stickyRemove = false;
+    let stickyFirstPass = false;
 
     let nodeTopPosition = Number(window.getComputedStyle(node).top.replace('px', ''));
     let nodeHeight = Number(window.getComputedStyle(node).height.replace('px', ''));
@@ -29,10 +29,9 @@
         
         if (scrollPos > nodeTopPos && !isNodeSticky) {
             node.classList.add(stickyClass);
-            stickyRemove = false;
-        } else if (scrollPos > (nodeTopPos + nodeHeight + 6) && !stickyRemove) {
-            stickyRemove = true;
-        } else if (scrollPos <= (nodeTopPos + nodeHeight) && isNodeSticky && stickyRemove) {
+        } else if (scrollPos > (nodeTopPos + nodeHeight + 6) && !stickyFirstPass) {
+            stickyFirstPass = true;
+        } else if (scrollPos <= (nodeTopPos + nodeHeight) && isNodeSticky) {
             node.classList.remove(stickyClass);
         }
     }
